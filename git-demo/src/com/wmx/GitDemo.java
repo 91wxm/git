55fa75cfd256d6fbc5e8c057b50c88332e08e84e
wmx <2935583905@qq.com>
2022-10-28 21:59:00 +0800

    master third commit

--- git-demo/src/com/wmx/GitDemo.java
+++ git-demo/src/com/wmx/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("master 第一次提交");
         System.out.println("master 第二次提交");
+        System.out.println("master 第三次提交");
     }
 }
