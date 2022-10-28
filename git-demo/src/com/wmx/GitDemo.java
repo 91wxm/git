6f76ebbbd06d1f2070410fdd7e54fcee15154cd9
wmx <2935583905@qq.com>
2022-10-28 21:45:18 +0800

    master second commit

--- git-demo/src/com/wmx/GitDemo.java
+++ git-demo/src/com/wmx/GitDemo.java
@@ -3,5 +3,6 @@ package com.wmx;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("master 第一次提交");
+        System.out.println("master 第二次提交");
     }
 }
